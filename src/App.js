99072ba6dd15541bbdae6a1170069cffb7837723
Mario Carballo Zama <afk@ellugar.co>
2023-09-01 14:08:22 -0600

    feat: basic data fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://dxcnmehgippzsoygnrst.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4Y25tZWhnaXBwenNveWducnN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1OTQ2OTksImV4cCI6MjAwOTE3MDY5OX0.tm8pQ3LlsbUPgzzJnVsefe2w4ZV1Y7SjiENb73XsaeE"
+);
 
 function App() {
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    getTickets();
+  }, []);
+
+  async function getTickets() {
+    const { data } = await supabase.from("players").select("*");
+    setTickets(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {tickets.map((item) => {
+          return (
+            <li key={item.id}>
+              <span>
+                {item.gems_collected}/{item.gems_total}
+              </span>
+              <time>{item.created_at}</time>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
